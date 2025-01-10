8cdb0c9fd7519e3b51cd2c193d496984ed05d513
BellaVignoli <bellavignoli@gmail.com>
2025-01-10 01:46:26 +0000

    test dataset

--- ml-container/recommendation_model.py
+++ ml-container/recommendation_model.py
@@ -1,19 +1,11 @@
-import os
 from fpgrowth_py import fpgrowth
 import pickle
-from pathlib import Path
 
 # Carregar os dados do CSV (exemplo)
 import pandas as pd
 
 # Carregar o dataset
-
-dataset_path = os.getenv('DATASET_PATH', '/app/datasets')  # Valor padrão: '/app/datasets'
-dataset_name = os.getenv("DATASET_NAME", "2023_spotify_ds2.csv")
-# file_path = "/app/datasets/2023_spotify_ds1.csv"
-
-file_path = Path(dataset_path) / dataset_name
-
+file_path = "/app/datasets/2023_spotify_ds1.csv"
 df = pd.read_csv(file_path)
 
 # Pré-processamento dos dados (transformar playlists em listas de itens)
@@ -29,5 +21,3 @@ freqItemSet, rules = fpgrowth(itemSetList, minSupRatio=min_support, minConf=min_
 model_path = "/arq/recommendation_model.pickle"
 with open(model_path, 'wb') as f:
     pickle.dump(rules, f)
-
-print("Modelo gerado e salvo com sucesso!")
